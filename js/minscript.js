b974412f3c77c113b606584acbcd4bc5774da477
podluzhnyyK <podluzhnyy94@mail.ru>
2020-10-28 23:07:55 +0500

    вторая проверка

---
+++ js/minscript.js
@@ -0,0 +1,1 @@
+const popup=document.querySelector(".search-popup"),btn=document.querySelector(".btn-search");popup.classList.add("hidden-popup"),btn.onclick=function(){popup.classList.remove("hidden-popup"),popup.classList.add("animation-popup")};
